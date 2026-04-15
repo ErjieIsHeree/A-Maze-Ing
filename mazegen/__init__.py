e50b6754c65f670a717426287e5b8a08f73a42f8
ErjieIsHeree <erjieisheree@gmail.com>
2026-04-15 17:50:54 +0200

    <fix>(__init__): removed a-maze-ing<main> file from the init

--- mazegen/__init__.py
+++ mazegen/__init__.py
@@ -1,10 +1,8 @@
 
-from .a_maze_ing import create_maze_generator
 from .maze_generator import MazeFactory
 from .configuration import Configuration
 
 __all__ = [
-    "create_maze_generator",
     "MazeFactory",
     "Configuration"
 ]
