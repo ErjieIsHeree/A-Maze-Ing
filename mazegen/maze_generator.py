dd3cd9d918f954f04b759e1297173a642248416a
conzeta <lu.duran2.2.0@gmail.com>
2026-04-23 16:06:27 +0200

    DFS, GT Algorithms

--- mazegen/maze_generator.py
+++ mazegen/maze_generator.py
@@ -82,15 +82,23 @@ class Maze(BaseModel):
 class MazeGenerator(ABC):
     """The maze generator creator"""
     def __init__(self, config: MazeConfiguration) -> None:
-        """This is just the signature of a Maze Generator init"""
-        self.config = config
+        """
+        Initializes the maze generator with a specific configuration.
 
-        try:
-            self.validate_config()
-        except Exception as err:
-            print(f"[ERROR]: {err}")
-            sys.exit(1)
-        return
+        Args:
+            config (MazeConfiguration): Data-class containing maze dimensions,
+                entry/exit points, and the reproduction seed.
+        """
+        self.config = config
+        self.rng = random.Random(self.config.SEED)
+        self.skip_pattern = False
+        self.validate_config()
+        # try:
+        #     self.validate_config()
+        # except Exception as err:
+        #     print(f"[ERROR]: {err}")
+        #     sys.exit(1)
+        # return
 
     @abstractmethod
     def generate(self) -> Maze:
@@ -100,56 +108,6 @@ class MazeGenerator(ABC):
             Maze: The Maze data-class with a Maze information
         """
         ...
-
-    @abstractmethod
-    def validate_config(self) -> None:
-        """Validates if maze can be created with the actual configuration"""
-        ...
-    pass
-
-
-class ExampleMazeGenerator(MazeGenerator):  # !! BORRAR ANTES DE ENTREGAR
-    def __init__(self, config: MazeConfiguration) -> None:
-        super().__init__(config)
-        return
-
-    def generate(self) -> Maze:
-        return Maze(maze_map="Hola", entry=(1, 1), exit=(16, 2))
-
-    def validate_config(self) -> None: ...
-    pass
-
-
-class DFSMazeGenerator(MazeGenerator):
-    """
-    Maze generator that implements the Depth-First Search (DFS) algorithm,
-    also known as the Recursive Backtracker.
-    This generator produces perfect mazes by carving paths into a grid of
-    fully closed cells, ensuring full connectivity without loops.
-
-    Attributes:
-        MOVEMENTS (dict): Mapping of cardinal directions to grid deltas and
-        bitwise wall removal values (N=1, E=2, S=4, W=8).
-    """
-    MOVEMENTS: dict[str, tuple[int, int, int, int]] = {
-            "E": (0, 1, -2, -8),
-            "S": (1, 0, -4, -1),
-            "W": (0, -1, -8, -2),
-            "N": (-1, 0, -1, -4)
-    }
-
-    def __init__(self, config: MazeConfiguration) -> None:
-        """
-        Initializes the DFS generator with a specific configuration.
-
-        Args:
-            config (MazeConfiguration): Data-class containing maze dimensions,
-                entry/exit points, and the reproduction seed.
-        """
-        super().__init__(config)
-        self.rng = random.Random(self.config.SEED)
-        self.skip_pattern = False
-        self.validate_config()
 
     def validate_config(self) -> None:
         """
@@ -219,6 +177,70 @@ class DFSMazeGenerator(MazeGenerator):
             lines.append(hex_row)
         return "\n".join(lines) + "\n"
 
+    def _add_loops(self, grid: list[list[int]],
+                   ft_pattern: list[list[bool]]) -> None:
+        """
+        Adds cycles to the maze by removing L extra walls
+        between adjacent cells.
+
+        Args:
+            grid (list[list[int]]): The maze grid to modify.
+            ft_pattern (list[list[bool]]): Matrix where True is a '42' cell.
+        """
+        L = max(1, int((self.config.WIDTH * self.config.HEIGHT) * 0.02))  # probar y cambiar si hace falta. igual hacer distinción labs pequeños/grandes
+        extra_corrs = 0
+        while extra_corrs < L:
+            y = self.rng.randint(0, self.config.HEIGHT - 1)
+            x = self.rng.randint(0, self.config.WIDTH - 1)
+            if ft_pattern[y][x]:
+                continue
+
+            valid_neighbours = []
+            for direction, (dy, dx, c_w, n_w) in self.MOVEMENTS.items():
+                ny, nx = y + dy, x + dx
+                if (0 <= ny < self.config.HEIGHT
+                   and 0 <= nx < self.config.WIDTH):
+                    if not ft_pattern[ny][nx]:
+                        if grid[y][x] & abs(c_w):
+                            valid_neighbours.append((ny, nx, c_w, n_w))
+
+            if valid_neighbours:
+                ny, nx, c_w, n_w = self.rng.choice(valid_neighbours)
+                grid[y][x] += c_w
+                grid[ny][nx] += n_w
+                extra_corrs += 1
+
+
+class ExampleMazeGenerator(MazeGenerator):  # !! BORRAR ANTES DE ENTREGAR
+    def __init__(self, config: MazeConfiguration) -> None:
+        super().__init__(config)
+        return
+
+    def generate(self) -> Maze:
+        return Maze(maze_map="Hola", entry=(1, 1), exit=(16, 2))
+
+    def validate_config(self) -> None: ...
+    pass
+
+
+class DFSMazeGenerator(MazeGenerator):
+    """
+    Maze generator that implements the Depth-First Search (DFS) algorithm,
+    also known as the Recursive Backtracker.
+    This generator produces perfect mazes by carving paths into a grid of
+    fully closed cells, ensuring full connectivity without loops.
+
+    Attributes:
+        MOVEMENTS (dict): Mapping of cardinal directions to grid deltas and
+        bitwise wall removal values (N=1, E=2, S=4, W=8).
+    """
+    MOVEMENTS: dict[str, tuple[int, int, int, int]] = {
+            "E": (0, 1, -2, -8),
+            "S": (1, 0, -4, -1),
+            "W": (0, -1, -8, -2),
+            "N": (-1, 0, -1, -4)
+    }
+
     def _run_dfs(self, grid: list[list[int]],
                  visited: list[list[bool]]) -> None:
         """
@@ -253,39 +275,6 @@ class DFSMazeGenerator(MazeGenerator):
             else:
                 stack.pop()
 
-    def _add_loops(self, grid: list[list[int]],
-                   ft_pattern: list[list[bool]]) -> None:
-        """
-        Adds cycles to the maze by removing L extra walls
-        between adjacent cells.
-
-        Args:
-            grid (list[list[int]]): The maze grid to modify.
-            ft_pattern (list[list[bool]]): Matrix where True is a '42' cell.
-        """
-        L = max(1, int((self.config.WIDTH * self.config.HEIGHT) * 0.02))  # probar y cambiar si hace falta
-        extra_corrs = 0
-        while extra_corrs < L:
-            y = self.rng.randint(0, self.config.HEIGHT - 1)
-            x = self.rng.randint(0, self.config.WIDTH - 1)
-            if ft_pattern[y][x]:
-                continue
-
-            valid_neighbors = []
-            for direction, (dy, dx, c_w, n_w) in self.MOVEMENTS.items():
-                ny, nx = y + dy, x + dx
-                if (0 <= ny < self.config.HEIGHT
-                   and 0 <= nx < self.config.WIDTH):
-                    if not ft_pattern[ny][nx]:
-                        if grid[y][x] & abs(c_w):
-                            valid_neighbors.append((ny, nx, c_w, n_w))
-
-            if valid_neighbors:
-                ny, nx, c_w, n_w = self.rng.choice(valid_neighbors)
-                grid[y][x] += c_w
-                grid[ny][nx] += n_w
-                extra_corrs += 1
-
     def generate(self) -> Maze:
         """
         Orchestrates the maze generation process.
@@ -309,10 +298,75 @@ class DFSMazeGenerator(MazeGenerator):
 
         if not self.config.PERFECT:
             self._add_loops(grid, ft_pattern)
-        maze_map_str = self._grid_to_hex_str(grid)
 
         return Maze(
-            maze_map=maze_map_str,
+            maze_map=self._grid_to_hex_str(grid),
+            entry=self.config.ENTRY,
+            exit=self.config.EXIT
+        )
+
+
+class GTMazeGenerator(DFSMazeGenerator):
+    """
+    Growing Tree Generator.
+    Inherits movements and utility methods from DFSMazeGenerator.
+    """
+    def _run_growing_tree(self, grid: list[list[int]], visited: list[list[bool]]) -> None:
+        start_x, start_y = self.config.ENTRY
+        active_cells = [(start_y, start_x)]
+        visited[start_y][start_x] = True
+        steps_in_current_path = 0
+        curr_y, curr_x = start_y, start_x
+
+        while active_cells:
+            if steps_in_current_path >= 4:
+                idx = self.rng.randint(0, len(active_cells) - 1)
+                curr_y, curr_x = active_cells[idx]
+                steps_in_current_path = 0
+            else:
+                curr_y, curr_x = active_cells[-1]
+
+            neighbours = []
+            for direction, (dy, dx, c_w, n_w) in self.MOVEMENTS.items():
+                ny, nx = curr_y + dy, curr_x + dx
+                if 0 <= ny < self.config.HEIGHT and 0 <= nx < self.config.WIDTH:
+                    if not visited[ny][nx]:
+                        neighbours.append((ny, nx, c_w, n_w))
+
+            if neighbours:
+                ny, nx, c_w, n_w = self.rng.choice(neighbours)
+                grid[curr_y][curr_x] += c_w
+                grid[ny][nx] += n_w
+                visited[ny][nx] = True
+                active_cells.append((ny, nx))
+                steps_in_current_path += 1
+            else:
+                active_cells.remove((curr_y, curr_x))
+                steps_in_current_path = 4
+
+    def generate(self) -> Maze:
+        """
+        Overrides the generate method to use Growing Tree instead of DFS.
+        Returns:
+            Maze: The completed maze object containing the map and metadata.
+        """
+        grid = [[0xF for _ in range(self.config.WIDTH)]
+                for _ in range(self.config.HEIGHT)]
+        visited = [[False for _ in range(self.config.WIDTH)]
+                   for _ in range(self.config.HEIGHT)]
+
+        if not self.skip_pattern:
+            for y, x in self._get_42_coords():
+                if 0 <= y < self.config.HEIGHT and 0 <= x < self.config.WIDTH:
+                    visited[y][x] = True
+        ft_pattern = copy.deepcopy(visited)
+        self._run_growing_tree(grid, visited)
+
+        if not self.config.PERFECT:
+            self._add_loops(grid, ft_pattern)
+
+        return Maze(
+            maze_map=self._grid_to_hex_str(grid),
             entry=self.config.ENTRY,
             exit=self.config.EXIT
         )
@@ -323,6 +377,7 @@ class Algorithms(Enum):
     DEFAULT = ExampleMazeGenerator
     EXAMPLE = ExampleMazeGenerator  # !! BORRAR ANTES DE ENTREGAR
     DFS = DFSMazeGenerator
+    GT = GTMazeGenerator
     pass
 
 
